7083b7dbbf00580b148538465df39f2eaf6946ee
Sy Traore <stephanetraore46@gmail.com>
2024-03-16 00:44:31 -0400

    Revert "start part 2"
    This reverts commit c0226bc7dad28accde87621e22882e92a4c130b6.

--- flashcards/src/App.jsx
+++ flashcards/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './components/Card'
-import CardForm from './components/CardForm'
 
 const App = () => {
   const questions = [
@@ -13,30 +12,13 @@ const App = () => {
   ]
   
   const [currentCard, setCurrentCard] = useState(0)
-  const [userAnswer, setUserAnswer] = useState('')
-  const [isAnswerShown, setIsAnswerShown] = useState(false)
-  const [feedback, setFeedback] = useState(null)
 
   const handleNextClick = () => {
     setCurrentCard((index) => (index+1) % questions.length)
-    setUserAnswer('') // Clear user answer for next card
-    setIsAnswerShown(false)
-    setFeedback(null)
   }
   
   const handlePrevClick =() => {
     setCurrentCard((index) => (index - 1 + questions.length) % questions.length)
-    setUserAnswer('') // Clear user answer for next card
-    setIsAnswerShown(false)
-    setFeedback(null)
-  }
-
-
-  const handleCheckAnswer = (e) => {
-    e.preventDefault(); // Prevent default form submission
-    const isCorrect = userAnswer.toLowerCase() === questions[currentCard].answer.toLowerCase();
-    setFeedback(isCorrect);
-    setIsAnswerShown(true);
   }
   return(
     <div className="App">
@@ -50,10 +32,11 @@ const App = () => {
       <div className="container">
         <div className = "card-view">
           
-          <Card question={questions[currentCard].question} answer={questions[currentCard].answer} isAnswerShown={isAnswerShown} feedback={feedback} />
-          { !isAnswerShown && <CardForm userAnswer={userAnswer} setUserAnswer={setUserAnswer} onCheckAnswer={handleCheckAnswer}/> }
+          <Card question={questions[currentCard].question} answer={questions[currentCard].answer} />
         </div>
         
+
+
       </div>
       <div className = "button-container">
           <button onClick={handlePrevClick}>Previous</button>
